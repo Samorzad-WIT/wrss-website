024022ae116fdf0ce862a810a3f56a3f77970146
alsjkdflkw <289861@student.pwr.edu.pl>
2026-04-01 09:56:33 +0200

    Init, nic innego

---
+++ react-ts-vite-app/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="container">
+      <section className="hero">
+        <h1 className="title">Strona samorządowa</h1>
+        <p className="subtitle">Witaj na oficjalnej stronie Samorządu Studenckiego.</p>
+      </section>
+    </main>
+  )
+}
+
+export default App
